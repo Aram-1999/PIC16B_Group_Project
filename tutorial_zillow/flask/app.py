f8e9c365d43af4e3b21a9b7a3b76f5ffd2212d86
Aram Torosyan <aramtorosyan1999@gmail.com>
2023-03-02 15:44:51 -0800

    created flask app

--- tutorial_zillow/flask/app.py
+++ tutorial_zillow/flask/app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('base.html')
+
+@app.route('/data_collection')
+def data_collection():
+    return render_template('data_collection.html')
+
+@app.route('/visualization')
+def visualization():
+    return render_template('visualization.html')
